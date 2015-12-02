631876cea8ccd94cafbb62f4e1022c9e7f2d0443
Jan Sorensen <sorensenjan@gmail.com>
2015-12-02 15:01:56 +0100

    Update to zsh 5.0.8

--- modules/unuxus/init.zsh
+++ modules/unuxus/init.zsh
@@ -12,7 +12,7 @@ if [[ "$OSTYPE" == darwin* ]] ; then
 
     if [[ $homebrewZsh == $SHELL ]] ; then
         # Use git completion from system zsh.
-        local git_completion=/usr/share/zsh/5.0.5/functions/_git
+        local git_completion=/usr/share/zsh/5.0.8/functions/_git
         if [[ -f $git_completion ]] ; then
             local fpathDir=${0:h}/unuxus_fpath
             command rm -rf $fpathDir
